909a8cadace18a10aa3dfa3e1673fd05fa9496b0
Julio Lima <lima.julio.cs@gmail.com>
2022-11-02 13:25:19 -0300

    mobile: day 1, 2 and 3

---
+++ src/screens/Pools.tsx
@@ -0,0 +1,8 @@
+import { VStack } from 'react-native';
+
+export function Pools() {
+
+  return(
+    <VStack flex={1} bgColor="gray.900"></VStack>
+  );
+}
---
+++ src/screens/Signin.tsx
@@ -0,0 +1,28 @@
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from '../hooks/useAuth';
+
+import { Button } from "../components/Button";
+
+import Logo from '../assets/logo.svg';
+
+export function Signin() {
+
+  const { signIn, user } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={240} height={40}/>
+      <Button 
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+        onPress={signIn}
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além{'\n'}do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  );
+}
